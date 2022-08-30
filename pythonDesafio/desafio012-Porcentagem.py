15d0ad47b8ad57e590c597927d8cbdbf1bca06cb
ribeirosacarlos <83379587+ribeirosacarlos@users.noreply.github.com>
2022-08-29 21:35:26 -0300

    Delete desafio012-Porcentagem.py

--- pythonDesafio/desafio012-Porcentagem.py
+++
@@ -1,6 +0,0 @@
-produto = float(input('Digite o preço do produto: R$'))
-porcentagem = (5 / 100) * produto
-novoPreco = produto - porcentagem
-print('O preço do produto com desconto é de R${} reais.'.format(novoPreco))
-
-
