30692eb35ad9a010a5b19d3f09359d6183a70faf
ribeirosacarlos <83379587+ribeirosacarlos@users.noreply.github.com>
2022-08-29 21:35:36 -0300

    Delete desafio013-aumentoCorreto.py

--- pythonDesafio/desafio013-aumentoCorreto.py
+++
@@ -1,3 +0,0 @@
-salario = float(input('Qual é o salario do funcionario? R$'))
-novo = salario + (salario * 15 / 100)
-print('O funcionario recebeu um aumento de R${}, para R${}' .format(salario, novo))
