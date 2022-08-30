f57cdc249da958f702d1dd6f5e326143012d485a
ribeirosacarlos <83379587+ribeirosacarlos@users.noreply.github.com>
2022-08-29 21:34:30 -0300

    Delete desafio015-aluguelCarros.py

--- pythonDesafio/desafio015-aluguelCarros.py
+++
@@ -1,4 +0,0 @@
-km = float(input('Qual a quantidade de km percorridos?'))
-dias = int(input('Foram quantos dias de aluguel? '))
-pago = (km * 0.15) + (dias * 60)
-print('O preço a se pagar pelo aluguel de {} dias, e {} km, é de R${:.2f}!'.format(dias, km, pago))
