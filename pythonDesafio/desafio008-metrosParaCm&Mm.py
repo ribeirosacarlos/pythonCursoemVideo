221d5a3bbcb10bd2fc42e76259740cc269f4594b
ribeirosacarlos <83379587+ribeirosacarlos@users.noreply.github.com>
2022-08-29 21:34:56 -0300

    Delete desafio008-metrosParaCm&Mm.py

--- pythonDesafio/desafio008-metrosParaCm&Mm.py
+++
@@ -1,5 +0,0 @@
-medida = float(input('Digite um valor em metros: '))
-cm = medida * 100
-mm = medida * 1000
-
-print('{} metros, é igual a {:.0f}cm.\n{} metros, é igual a {:.0f}mm.'.format(medida, cm, medida, mm))
