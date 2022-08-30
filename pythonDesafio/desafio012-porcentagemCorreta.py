8977dd567c9c80e3a8f183d67dcf3e5b77937b4d
ribeirosacarlos <83379587+ribeirosacarlos@users.noreply.github.com>
2022-08-29 21:35:30 -0300

    Delete desafio012-porcentagemCorreta.py

--- pythonDesafio/desafio012-porcentagemCorreta.py
+++
@@ -1,3 +0,0 @@
-preço = float(input('Qual o preço do produto? R$'))
-novo = preço - (preço * 5/100)
-print('O produto que custava R${}, na promoção com desconto de 5% vai custar R${}'.format(preço, novo))
