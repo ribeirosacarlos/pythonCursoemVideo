6d393b721c3dce725aee0c3be6d5294bb7c3caeb
ribeirosacarlos <83379587+ribeirosacarlos@users.noreply.github.com>
2022-08-29 21:35:42 -0300

    Delete desafio013-aumentoPorcetagem.py

--- pythonDesafio/desafio013-aumentoPorcetagem.py
+++
@@ -1,8 +0,0 @@
-salario = float(input('Digite o salário do funcionário: '))
-porcentagem = (15 / 100) * salario
-aumento = salario + porcentagem
-print('O salário desse funcionário vai para R${} .'.format(aumento))
-
-
-
-
